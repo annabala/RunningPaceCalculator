35cf3caa964b03cbb44df9f8795d5b1d5aea0ea3
Anna Bała <aniabala92@gmail.com>
2018-07-24 22:33:06 +0200

    parseInt added

--- script.js
+++ script.js
@@ -9,20 +9,25 @@ let distance, hour, minutes, seconds;
 
 function calculatePace() {
     
-    distance = document.getElementById('distance-input').value;
-    hour = document.getElementById('hour-input').value;
-    minutes = document.getElementById('minutes-input').value;
-    seconds = document.getElementById('seconds-input').value;
+    distance = parseInt(document.getElementById('distance-input').value);
+    hour = parseInt(document.getElementById('hour-input').value);
+    minutes = parseInt(document.getElementById('minutes-input').value);
+    seconds = parseInt(document.getElementById('seconds-input').value);
     
     let hourValue = hour * 60;
+    console.log(hourValue);
+    let minutesValue = minutes * 1;
     let secondsValue = seconds / 60;
-    let distanceValue = distance * 1000;
+    let distanceValue = distance * 1;
+    console.log(distanceValue);
     let finalResult = 0;
     let time = 0;
     let resultTime = 0;
     
-    time = hourValue + minutes + secondsValue;
+    time = hourValue + minutesValue + secondsValue;
+    console.log(time);
     finalResult = (time / distanceValue);
     resultTime.innerHTML = finalResult;
 }
-document.getElementById('show-result').addEventListener('submit', calculatePace);
+
+document.getElementById('show-result').addEventListener('click', calculatePace);
