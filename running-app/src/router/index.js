c21a68604d8dc9780333184b93078b148dfcac33
[Anna Bała] <[a.bala@cormo.pl]>
2019-11-05 23:02:14 +0100

    added gallery component and view to router

--- running-app/src/router/index.js
+++ running-app/src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "" */ '../views/App-Results.vue')
+  },
+  {
+    path: '/app-gallery',
+    name: 'App-Gallery',
+    component: () => import(/* webpackChunkName: "" */ '../views/App-Gallery.vue')
   }
 ]
 
---
+++ running-app/src/views/App-Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-gallery-view">
+    <AppGallery/>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import AppGallery from '@/components/AppGallery.vue'
+
+export default {
+  name: 'app-gallery',
+  components: {
+    AppGallery
+  }
+}
+</script>
