b8d217635acafc399efd4ffcb9c23c1208a05e45
[Anna Bała] <[a.bala@cormo.pl]>
2019-11-05 23:02:30 +0100

    writing methods for component

--- running-app/src/components/AppResults.vue
+++ running-app/src/components/AppResults.vue
@@ -18,21 +18,50 @@
           </div>
         </div>
       </div>
-      <button type="button">Add</button>
+      <button type="button" @click="addItem()">Add</button>
     </form>
-    <ul id="score-list" class="grid-el">
-      <li class="score-list-item"></li>
-    </ul>
+    <div id="score-list" class="best-results--list">
+      <span class="score-list-item"></span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AppResults'
+  name: 'AppResults',
+  methods: {
+    addItem: function() {
+            let elVal, stringValues;
+            let listItem = document.querySelector('.score-list-item');
+            let singleItemValues = document.querySelectorAll('.item-value');
+            let valueArray = Array.prototype.slice.call(singleItemValues);
+            let i = 1;
+            let getValues = valueArray.map((el) => {
+                return elVal = el.value;
+            });
+            if(elVal) {
+                stringValues = getValues.join(', ');
+                listItem.innerHTML = `${i++}. ${stringValues}`;
+            } else {
+              alert("Can't add empty line :(, fill inputs");
+            }
+        }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.best-results {
+    display: flex;
+    flex-direction: column;
+    max-width: 500px;
+    margin: 0 auto;
+    justify-content: center;
+    &--list {
+    margin: 2rem 0 0 0;
+    font: 600 1.5rem/2.5rem 'Quicksand';
+    color: #fff183;
+  }
+}
 </style>
